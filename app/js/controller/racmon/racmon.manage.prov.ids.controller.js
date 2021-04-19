2be852f48acf831ccaaaacb69f17b57231a701c8
xiyu <xiaoming.yu@monash.edu>
2021-04-19 12:57:05 +1000

    updated the error handling when updated provision id

--- app/js/controller/racmon/racmon.manage.prov.ids.controller.js
+++ app/js/controller/racmon/racmon.manage.prov.ids.controller.js
@@ -31,59 +31,84 @@
             });
         }
 
-        vm.open_popup_window = function(storage_req) {
+        vm.open_popup_window = function (storage_req) {
             vm.show_popup_window = true;
             vm.selected_stor_req = storage_req;
             vm.get_recent_provision_ids(vm.selected_stor_req.storage_product.name);
         };
 
-        vm.close_popup_window = function() {
+        vm.close_popup_window = function () {
             vm.show_popup_window = false;
             clear_prov_id_popup();
         };
 
-        vm.change_prov_id = function() {
+        vm.change_prov_id = function () {
             vm.show_popup_window = false;
         };
 
-        vm.change_prov_id = function() {
+        vm.change_prov_id = function () {
             if (vm.new_prov_id) {
                 CramsApiService.updateProvisionID(vm.selected_stor_req.id, vm.selected_stor_req.storage_product, vm.new_prov_id).then(function (response) {
-                   if (response.success) {
-                       FlashService.Success("Success - Provision ID changed");
-                       loadAllocationRequest(vm.project_id);
-                       // reset the popup
-                       clear_prov_id_popup();
-                       vm.show_popup_window = false;
-                   } else {
-                       let error_obj = response.data;
-                       let error_msg = "";
-                       if (error_obj.provision_id.length === 1) {
-                           error_msg = error_obj.provision_id[0];
-                       } else {
-                           for (let i=0; i< error_obj.provision_id.length; i++) {
-                               error_msg = error_msg + ', ' + error_obj.provision_id[i];
-                           }
-                       }
-
-                       FlashService.DisplayError(error_msg, response.data);
-                   }
+                    if (response.success) {
+                        check_sp_provision_response(response.data);
+                        vm.show_popup_window = false;
+                    } else {
+                        var error_msg = 'Faild to update the provision id ';
+                        FlashService.DisplayError(error_msg, response.data);
+                    }
                 });
             } else {
                 vm.prov_id_invalid = true;
             }
         };
 
-        vm.get_recent_provision_ids = function(product_name) {
-            CramsApiService.getRecentProvId(product_name).then(function (response) {
-               if (response.success) {
-                   vm.available_prov_ids = response.data;
+        function check_sp_provision_response(resp_data) {
+            vm.resp_errors = [];
+            var success = resp_data.success;
+            if (success === false) {
+                var errors = resp_data.errors;
+                if (errors !== null) {
+                    var non_field_errors = _.pick(errors, 'non_field_errors');
+                    if (!_.isEmpty(non_field_errors)) {
+                        vm.resp_errors.push(non_field_errors.non_field_errors[0]);
+                    }
+                    var sp = errors.storage_product;
+                    if (sp !== undefined) {
+                        var sp_non_field_errors = _.pick(sp, 'non_field_errors');
+                        if (!_.isEmpty(sp_non_field_errors)) {
+                            vm.resp_errors.push(sp_non_field_errors.non_field_errors[0]);
+                        }
+                    }
+                    var prov_id_errors = _.pick(errors, 'provision_id');
+                    if (!_.isEmpty(prov_id_errors)) {
+                        vm.resp_errors.push(prov_id_errors.provision_id[0]);
+                    }
+                }
+            }
 
-                   // add a unique index for the drop down
-                   for (let i = 0; i < vm.available_prov_ids.length; i++) {
-                       vm.available_prov_ids[i]["index"] = i;
-                   }
-               }
+            vm.sp_provision_error = false;
+            if (vm.resp_errors.length > 0) {
+                var msg = vm.resp_errors[0];
+                FlashService.DisplayError(msg, null);
+                $anchorScroll();
+            } else {
+                FlashService.Success("Success - Provision ID changed");
+                loadAllocationRequest(vm.project_id);
+                clear_prov_id_popup();
+                vm.show_popup_window = false;
+            }
+        }
+
+        vm.get_recent_provision_ids = function (product_name) {
+            CramsApiService.getRecentProvId(product_name).then(function (response) {
+                if (response.success) {
+                    vm.available_prov_ids = response.data;
+
+                    // add a unique index for the drop down
+                    for (let i = 0; i < vm.available_prov_ids.length; i++) {
+                        vm.available_prov_ids[i]["index"] = i;
+                    }
+                }
             });
         };
 
